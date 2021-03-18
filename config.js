10c396c643bfad53a6e3c613509f0181a0afbb72
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-03-17 23:58:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {	
 hostedBy: true, 
-prefix: "",
-OwnerID: "", 
-token: "",
-support: "",
+prefix: "!g",
+OwnerID: "790265200770809857", 
+token: "ODA2MjI2NTE0MjU2NDYxODY0.YBmW8w._2ABo75BpCseV5-QYbq8fv9cVm8",
+support: "4vthBEKHsB",
 basiclang: "en",
 color: "",
 reaction: "🎉", 
