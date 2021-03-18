8bac0e9397d42077cfcbbd99805e31cf5605e24e
zhenyar123 <70575666+zhenyar123@users.noreply.github.com>
2021-03-18 15:12:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 hostedBy: true, 
 prefix: "!g",
 OwnerID: "790265200770809857", 
-token: "ODA5NDM3MjAzMDEyNzgwMTAy.YCVFIw.8RIu2D9bTQf1YfkLis6bN5umss0",
+token: "ODIwMjQ4OTE5ODM2OTgzMzA3.YEyaVQ.SLKhV4nKN2fS2LSgvP7eqqLPu_E",
 support: "4vthBEKHsB",
 basiclang: "en",
 color: "",
